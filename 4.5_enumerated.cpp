f0182f1ef39059370bf35c77d7fb91ce2c4a8e78
冯磊 <lightionight@aol.com>
2018-10-17 11:25:26 +0800

    Update 4.5_enumerated.cpp

--- 4.5_enumerated.cpp
+++ 4.5_enumerated.cpp
@@ -1,7 +1,32 @@
-enum Color{
-  color_black,
-  color_bule,
-  color_white,
-  color_pink,
+#include <string>
+#include <iostream>
+//define enum
+enum MonsterRaces
+{
+  type_orcs,
+  type_gobins,
+  type_trolls,
+  type_ogres,
+  type_skeleton,
 };
 
+//chose test
+std::string getMonsterName (MonsterRaces monsterName)
+{
+  if (monsterName == type_orcs)
+    return std::string ("Orcs");
+  if (monsterName == type_gobins)
+    return std::string ("Gobins");
+  if (monsterName == type_trolls)
+    return std::string ("Trolls");
+  if (monsterName == type_skeleton)
+    return std::string ("Skeleton");
+  return std::string ("???");
+}
+//chose your Monster
+int main()
+{
+  MonsterRaces monsterName = type_gobins;
+  std::cout << "you're fight with " << getMonsterName(monsterName) << "\n";
+  return 0;
+}
