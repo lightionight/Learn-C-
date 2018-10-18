450a2c179e419ca7b0c511d64e7ecde837b4c31a
冯磊 <lightionight@aol.com>
2018-10-18 10:20:09 +0800

    Update 4.8_test.cpp
    maybe have some problems

--- 4.8_test.cpp
+++ 4.8_test.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 //define monster enum class
-enum class monster
+enum class monstertype
 {
   ogre,
   dragon,
   orc,
-  giantSpider,
+  giant_Spider,
   slime,
 };
 
@@ -15,13 +15,46 @@ enum class monster
 
 struct monsterValue
 {
+   monsterType type
    std::string monsterName;
    int         monsterHealth;
    double      monsterATK;
    double      monsterDEF;
 }
+std::string getmonsterType(monsterValue monster)
+{
+  if(monster.type == monsterType::ogre)
+    return "Ogre";
+  if(monster.type == monsterType::dragon)
+    return "Dragon";
+  if(monster.type == monsterType::orc)
+    return "Orc";
+  if(monster.type == monsterType::giant_Spider)
+    return "Giant Spider";
+  if(monster.type == monsterType::slime)
+    return "Slime";
+  
+  return "Unknown"
+}
 
-void printMonsterAttribute（）
+//打印怪物属性
 
-   
+void printMonsterAttribute（monsterValue monster）
+{
+    std::cout << "This " << getmonsterType(monster) << " is named " << monster.name << " has "
+              << "monster.Health" << ",have ATK " << "monster.ATK" << " point and DEF "
+              << "monster.DEF ";
+}
+
+int main()
+{
+  monstervalue ogre = { monsterType::Ogre,"tom" , 127 , 25 , 34};
+  monstervalue slime = { monsterType::slime,"jack" , 127 , 25 , 34};
+  
+  printMonsterAttribute(orge);
+  printMonsterAttribute(slime);
+  
+  return 0;
+}
+
    
