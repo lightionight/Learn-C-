0f6493ad7a3c56b943c535f5ecf7ac6f18d765dd
冯磊 <lightionight@aol.com>
2018-10-19 09:31:30 +0800

    Update 5.3_q1.cpp

--- 5/5.3_q1.cpp
+++ 5/5.3_q1.cpp
@@ -1,27 +1,38 @@
 #include <string>
 #include <iostream>
 
-enum calculateType
+int calculate(int x,int y, char op)
 {
-	a = "+",
-	b = "-",
-	c = ""
-}
-
-int calculate(int x,int y)
-{
-	using namespace std;
-	cout << "Enter x value";
-	cin >> x;
-	cout << '\n';
-	cout << "Enter y value";
-	cin >> y;
-	
-	return x+y;
+    switch (op)
+    {
+        case "+":
+            return x + y;
+        case "-":
+            return x - y;
+        case "*":
+            return x * y;
+        case "%":
+            return x % y;
+        default :
+            std::cout::<< "calculate fution unknow \n";
+        return 0;
+    }
 }
 
 int main()
 {
-	calculate();
-	std::cout << ""
+	std::cout << "Enter x value ";
+    int x;
+    std::cin >> x;
+
+    std::cout << "Enter y value ";
+    int y;
+    std::cin >> y;
+
+    std::cout << "Enter a mathematical opratar :";
+    char op;
+    std::cin >> op;
+
+    std::cout << x << " " << op << " " << y << " is " << calculate(x, y, op) << "\n";
+    return 0;
 }
