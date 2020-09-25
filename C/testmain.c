f4ad23519f21bd79af86141cc41048c34fa0dac0
lightionight <16332827+lightionight@users.noreply.github.com>
2020-09-25 11:09:29 +0800

    update

--- C/testmain.c
+++ C/testmain.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 //#include "PointerGetInput.c"
 
-
+void maxArea(int arr[], int *result);
 
 
 int main(){
-    
-    //int *pn, result;
-    //scanf("%d", *pn);
-    //result = getint(pn);
-    //printf("%d", result);
-    int a = 5;
-    int *p;
-    p = &a;
-    printf("0X%X, value is : %d", p, *p);
-    
-    int A[] = {31, 41, 59, 26, 41, 58};
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    int arr[9] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    int result = 0;
+    maxArea (arr, &result);
+    printf("The max Area is %d.\n", result);
     return 0;
+}
+
+void maxArea(int arr[], int *result){
+    int *i, *j, *storeValue;
+    i = arr;
+    j = i + 1;
+    storeValue = result;
+    int temp;
+    while (i == i++){
+        while(j == j++){
+            if(*i < *j)
+                temp = *i * (j - i);
+            else
+                temp = *j * (j - i);
+            if(temp > *storeValue)
+                *storeValue = temp;
+        }
+    }
+
 }
