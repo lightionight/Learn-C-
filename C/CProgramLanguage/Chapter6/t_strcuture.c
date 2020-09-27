aa7fbd9e21ecb316c22cacd1e4c6f1ab3537cb1b
lightionight <16332827+lightionight@users.noreply.github.com>
2020-09-27 17:38:31 +0800

    learn three chapter

---
+++ C/CProgramLanguage/Chapter6/t_strcuture.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//#define rect struct rect
+
+struct position
+{
+    int x;
+    int y;
+};
+
+struct rect
+{
+    struct position position1;
+    struct position position2;
+};
+
+void InitRect(struct rect *pp, int, int, int);
+
+int main(){
+    struct rect addpp, *pp;
+    pp = &addpp;
+    InitRect(pp, 4, 5, 2);
+    printf("This is : %d\n", pp->position1.x);
+    return 0;
+}
+
+void InitRect(struct rect *pp, int x, int y, int pointN ){
+    pp->position1.x = x;
+}
+
