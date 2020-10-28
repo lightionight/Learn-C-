aab761f614091459eb6d7d4fb07fe45b1de63be3
lightionight <16332827+lightionight@users.noreply.github.com>
2020-10-28 17:38:36 +0800

    figura out array

---
+++ Cpp/TheCppProgrammingLanguage_3rdEdition/2_BasicFacilities/Chapter5_PointerArrayStructure/5.9.11.cpp
@@ -0,0 +1,40 @@
+/**
+ * 5.9.11. P105
+ * Read a sequence of words from input.
+ * Use Quit as aword that terminates the input.
+ * Print the words in the order they were enterd.
+ * Don't Print word Twice.
+ * Modifity the program sort the words before Print them.
+*/
+/******************************************************
+ * AUTHOR: FENG
+ * TIME：2020.10.28
+ * ***************************************************/
+
+#include "cppBaseInclude.h"
+#include <string>
+#include <array>
+void getInput(std::string& test);
+
+int main(){
+    std::string data;
+    getInput(data);
+
+}
+
+
+
+
+void getInput(std::string& test)
+{
+    std::cin >> test;
+    
+}
+
+void sortString(std::string& input)
+{
+    for(int i = 0; i < input.length(); i++)
+    {
+        if((input[i]))
+    }
+}
