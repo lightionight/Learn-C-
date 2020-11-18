07efea014b8dadffefd7484a4db5afdc2be42b4c
lightionight <16332827+lightionight@users.noreply.github.com>
2020-11-18 22:24:02 +0800

    test

---
+++ CSharp/EssentialCSharp/08_Interface/Interface_test1.cs
@@ -0,0 +1,9 @@
+public interface ISample
+{
+    private static string _field;
+    public static Field{
+        get => _field;
+        private set => _field = value;
+        static ISamplein
+    }
+}
--- CSharp/EssentialCSharp/13_DelagatesAndLambdaExpressions/DelagetesClass_Intro.cs
+++ CSharp/EssentialCSharp/13_DelagatesAndLambdaExpressions/DelagetesClass_Intro.cs
@@ -3,4 +3,4 @@ NAME : Delagates AND Lambda Expressions
 AUTHOR : F3NG
 DATE : 2020. 11.04
 ************************************************/
-//Lambda expression Also CAll Anonymous function(匿名函数或者匿名方法).
+//Lambda expression Also CAll Anonymous function(匿名函数或者匿名方法)
